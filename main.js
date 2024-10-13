bcd43378b5354b2452e95c6ec72eeee40c00e401
yusup0206 <yusuph0206@gmail.com>
2024-10-13 21:39:59 +0430

    mobile menu

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
-// // menu
-// const menu = document.querySelector('.menu');
-// const openMenu = document.querySelector('.openMenu');
-// const closeMenu = document.querySelector('.closeMenu');
-// const menuLink = document.querySelectorAll('.menuLink');
+// menu
+const menu = document.querySelector("#menu");
+const openMenu = document.querySelector("#openMenu");
+const closeMenu = document.querySelector("#closeMenu");
 
-// openMenu.addEventListener('click', () => {
-//   menu.classList.add('active');
-// });
+openMenu.addEventListener("click", () => {
+  menu.classList.add("active");
+});
 
-// closeMenu.addEventListener('click', () => {
-//   menu.classList.remove('active');
-// });
-
-// menuLink.forEach((e) => {
-//   e.addEventListener('click', () => {
-//     menu.classList.remove('active');
-//   });
-// });
+closeMenu.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
